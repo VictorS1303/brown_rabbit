d266b12d445539eab0af9f79b6f3614bfa8ead89
Victor Schweitz <victorschweitz@hotmail.com>
2024-10-04 11:30:14 +0200

    mobile_slider almost done (minor bug fixes)

--- js/main.js
+++ js/main.js
@@ -2,12 +2,85 @@ const mobileMenuToggleBtn = document.querySelector('#menu_toggle_btn')
 const mobileMenuContentContainer = document.querySelector('.mobile-menu-content-container')
 const mobileMenuSocialsContainer = document.querySelector('.socials-container')
 
+const slides = document.querySelectorAll('.slide')
+const prevSlideButton = document.querySelector('#prev_slide_btn')
+const nextSlideButton = document.querySelector('#next_slide_btn')
+const slideDescContainerArticles = document.querySelectorAll('.container.slide-desc-container article')
+console.log(slideDescContainerArticles)
+
 // EVENTLISTENERS //
 mobileMenuToggleBtn.addEventListener('click', toggleMobileMenu)
+prevSlideButton.addEventListener('click', prevSlide)
+nextSlideButton.addEventListener('click', nextSlide)
 
+
+// Mobile Menu //
+
+// Toggle Mobile Menu
 function toggleMobileMenu()
 {
     mobileMenuToggleBtn.classList.toggle('active')
     mobileMenuContentContainer.classList.toggle('active')
     mobileMenuSocialsContainer.classList.toggle('active')
 }
+
+
+
+
+
+// Slider //
+function nextSlide()
+{
+    // Get current class
+    const currentSlide = document.querySelector('.current')
+    const currentSlideArticle = document.querySelector('.current-slide-article')
+
+    // Remove current class
+    currentSlide.classList.remove('current')
+    currentSlideArticle.classList.remove('current-slide-article')
+
+    // Check for next slide
+    if (currentSlide.nextElementSibling)
+    {
+        // Add current to next sibling
+        currentSlide.nextElementSibling.classList.add('current')  
+        currentSlideArticle.nextElementSibling.classList.add('current-slide-article')
+    }
+    else
+    {
+      // Add current to start
+      slides[0].classList.add('current')
+      slideDescContainerArticles[0].classList.add('current-slide-article')
+    }   
+}
+  
+function prevSlide()
+{
+    // Get current class
+    const currentSlide = document.querySelector('.current')
+    const currentSlideArticle = document.querySelector('.current-slide-article')
+
+
+    // Remove current class
+    currentSlide.classList.remove('current')
+    currentSlideArticle.classList.remove('current-slide-article')
+
+
+    // Check for prev slide and previous slide article
+    if (currentSlide.previousElementSibling)
+    {
+        // Add current to prev sibling
+        currentSlide.previousElementSibling.classList.add('current')
+        currentSlideArticle.previousElementSibling.classList.add('current-slide-article')
+    }
+    else
+    {
+        // Add current to last slide
+        slides[slides.length - 1].classList.add('current')
+
+        // Add current to the last corresponding article
+        slideDescContainerArticles[slideDescContainerArticles.length - 1].classList.add('current-slide-article')
+    }
+
+}
+
--- requirements/requirements.md
+++ requirements/requirements.md
@@ -24,3 +24,7 @@ Apply responsivness, make sure that the page look good on the following media qu
 <h3>Pagination:</h3>
     You should be able to navigate through the articles. Make sure you have at least 15 articles.
     Use at least 1 loop and 1 click event
+
+
+<h3>Links</h3>
+<h4><a href="https://xd.adobe.com/view/e3a1c6ff-52f7-41ed-9e70-bad4e6e9b930-b6f2/screen/4f185c8a-5a94-439c-ba2e-ddcc68b8e92b/?fullscreen" target="_blank">Interactive Mobile Version</a></h4>
